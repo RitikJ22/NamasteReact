21861e2bdf605bcb65cac039b29955b9867e64bf
RitikJ22 <106678074+RitikJ22@users.noreply.github.com>
2023-01-04 16:16:41 +0530

    Session 1 Code

--- Session1/App.js
+++ Session1/App.js
@@ -1,4 +1,5 @@
 // Created Nested React Elements
+
     const heading1 = React.createElement( "h1", {  },  "Namaste Everyone !!!");
     const heading2 = React.createElement("h2", { }, "Namaste!!!");
     const container = React.createElement("div", {  }, [heading1, heading2]);
